3cc4780a61c076d0d982f0f90692d7325208d5bb
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-10-26 10:51:51 -0400

    I got four circles to move in different directions .

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,10 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var x1 : Int
+    var x2 : Int
+    var x3 : Int
+    var x4 : Int
     // This function runs once
     override init() {
         
@@ -16,19 +19,39 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
-        
+        x = 200
+        x1 = 200
+        x2 = 200
+        x3 = 200
+        x4 = 200
+        // No borders
+        canvas.drawShapesWithBorders = false
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        //clean up from the last time
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 40)
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
         // Change position
         x += 1
+        x1-=1
+        x2-=3
+        x3+=3
+        x4+=2
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.blue
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
+        canvas.fillColor = Color.yellow
+        canvas.drawEllipse(centreX: x1, centreY: 200, width: 50, height: 50)
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: x2, centreY: 300, width: 50, height: 50)
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(centreX: x3,centreY: 150, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x4,centreY: 350, width: 50, height: 50)
     }
+
     
 }

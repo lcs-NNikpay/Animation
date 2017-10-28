295d60213914283d5374ba84f1553e0d91c989c0
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-10-28 14:17:44 -0400

    I created 5 circles which move in order directions

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,10 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var x1 : Int
+    var x2 : Int
+    var x3 : Int
+    var x4 : Int
     // This function runs once
     override init() {
         
@@ -17,18 +20,33 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
-        
+        x1 = 250
+        x2 = 250
+        x3 = 250
+        x4 = 250
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
-        x += 1
+        x += 3
+        x1-=3
+        x2+=3
+        x3-=3
+        x4+=3
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
+        canvas.fillColor = Color.red
+         canvas.drawEllipse(centreX: x1, centreY: 350, width: 50, height: 50)
+        canvas.fillColor = Color.green
+         canvas.drawEllipse(centreX: x2, centreY: 450, width: 50, height: 50)
+        canvas.fillColor = Color.purple
+         canvas.drawEllipse(centreX: x3, centreY: 150, width: 50, height: 50)
+        canvas.fillColor = Color.orange
+         canvas.drawEllipse(centreX: x4, centreY: 50, width: 50, height: 50)
     }
     
 }

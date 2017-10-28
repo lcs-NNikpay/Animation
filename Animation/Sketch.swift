e1f61b4bc5c5eb0f1df1e7edaff7a7b66f0ac860
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-10-28 12:10:58 -0400

    I am still working on it

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,8 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    // change position of the circle
+    var dx : Int
     
     // This function runs once
     override init() {
@@ -17,16 +19,27 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        // set the change value
+        dx = 2
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
-        
+       
         // Change position
         x += 1
+        x += dx
+       
+        // make it bounce at right
+       
+        if x>500{//start of the block
+         dx = -2//move to the left
+        
+        }
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor=Color.black
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }

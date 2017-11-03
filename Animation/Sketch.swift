0f65f2e0806c74786ac6c459678ae409592f29df
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-11-03 11:27:43 -0400

    still needs progress

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,8 +9,11 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     var y : Int
+    var x2 : Int
+    var y2 : Int
     //change the position
     var a : Double
+    var a2 : Double
     
     // This function runs once
     override init() {
@@ -20,8 +23,11 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 0
+        x2 = 0
         y = 0
-        a = -500/(250 * 250)
+        y2 = 0
+        a = -100/(50 * 50)
+        a2 = -150/(75 * 75)
         
         
         
@@ -32,11 +38,14 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
-        y = Int(a * Double(x) * (Double(x)-500))
+        x2 += 1
+        y = Int(a * Double(x) * (Double(x)-100))
+        y2 = Int(a * Double(x2) * (Double(x2)-150))
         
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 20, height: 20)
+        canvas.drawEllipse(centreX: x2, centreY: y2, width: 20, height: 20)
         
     }
     

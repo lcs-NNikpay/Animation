1fa508efad5f6bf3a799689b6f77a1b9f81786b9
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-11-06 14:32:14 -0500

    many cannon balls

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -11,10 +11,15 @@ class Sketch : NSObject {
     var y : Int
     var x2 : Int
     var y2 : Int
+    var x3 : Int
+    var y3 : Int
+    var x4 : Int
+    var y4 : Int
     //change the position
     var a : Double
     var a2 : Double
-    
+    var a3 : Double
+    var a4 : Double
     // This function runs once
     override init() {
         
@@ -24,10 +29,16 @@ class Sketch : NSObject {
         // Set starting position
         x = 0
         x2 = 0
+        x3 = 0
         y = 0
         y2 = 0
+        y3 = 0
+        x4 = 0
+        y4 = 0
         a = -100/(50 * 50)
         a2 = -150/(75 * 75)
+        a3 = -200/(100 * 100)
+        a4 = -230/(115 * 115)
         
         
         
@@ -39,13 +50,20 @@ class Sketch : NSObject {
         // Change position
         x += 1
         x2 += 1
+        x3 += 1
+        x4 += 1
         y = Int(a * Double(x) * (Double(x)-100))
         y2 = Int(a * Double(x2) * (Double(x2)-150))
+        y3 = Int(a * Double(x3) * (Double(x3)-200))
+        y4 = Int(a * Double(x4) * (Double(x4)-230))
+        
         
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: y, width: 20, height: 20)
         canvas.drawEllipse(centreX: x2, centreY: y2, width: 20, height: 20)
+        canvas.drawEllipse(centreX: x3, centreY: y3, width: 20, height: 20)
+        canvas.drawEllipse(centreX: x4, centreY: y4, width: 20, height: 20)
         
     }
     
